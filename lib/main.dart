df2b47e26edc98b419e1bf18b9510f41c626e7ed
Shree-hari <harshmacc@gmail.com>
2019-12-20 17:55:26 +0530

    Converted stateless widgets to sateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -15,22 +15,47 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice = 6;
+  int rightDice = 3;
   @override
   Widget build(BuildContext context) {
+
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice1.png'), onPressed: () {},),
+              child: Image.asset('images/dice$leftDice.png'),
+              onPressed: () {
+                setState(() {
+                  leftDice = 5;
+                });
+              },
+            ),
           ),
           Expanded(
-            child: FlatButton(child: Image.asset('images/dice4.png'),
-              onPressed: () {},),
+            child: FlatButton(
+              child: Image.asset('images/dice$rightDice.png'),
+              onPressed: () {
+                setState(() {
+                  rightDice = 6;
+                });
+              },
+            ),
           )
         ],
       ),
     );
   }
+
 }
+
+
+
+
